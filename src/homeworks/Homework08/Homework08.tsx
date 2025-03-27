8dd8cdc4ae750b33f0a0f0e025a5de2e559fd641
IrinaKholkin <iryna1903@web.de>
2025-03-27 19:07:36 +0100

    add homework08

---
+++ src/homeworks/Homework08/Homework08.tsx
@@ -0,0 +1,27 @@
+import { ChangeEvent, useState } from "react"
+import Input from "../../components/Input/Input"
+import Button from "../../components/Button/Button";
+
+function Homework08(){
+const [password, setPassword] = useState<string>("");
+const [showPassword, setShowPassword] = useState<boolean>(false);
+
+const passwordChangeHandle = (event: ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+};
+
+const toogleShowPassword = () => {
+    setShowPassword(!showPassword);
+};
+
+    return (
+        <div className="homework08-container">
+        <h2>Change password</h2>
+        <Input name="passwordInput" label="Password" placeholder="Enter your password" type="password" value={password} onChange={passwordChangeHandle}/>
+        <Button name="SHOW PASSWORD" onClick={toogleShowPassword}></Button>
+        {showPassword && <div className="password-display">{password}</div>}
+        </div>);
+    }
+        
+
+export default Homework08;
