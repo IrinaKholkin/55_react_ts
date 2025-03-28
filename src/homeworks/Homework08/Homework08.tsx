d2df20b39e7d6ed1aaab62ffb798f55bf9842092
IrinaKholkin <iryna1903@web.de>
2025-03-28 11:09:42 +0100

    homework08-lesson

--- src/homeworks/Homework08/Homework08.tsx
+++ src/homeworks/Homework08/Homework08.tsx
@@ -1,25 +1,40 @@
 import { ChangeEvent, useState } from "react"
+
 import Input from "../../components/Input/Input"
 import Button from "../../components/Button/Button";
+import './styles.css'
 
 function Homework08(){
-const [password, setPassword] = useState<string>("");
-const [showPassword, setShowPassword] = useState<boolean>(false);
+const [passwordValue, setPasswordValue] = useState<string>("");
+const [isShowPassword, setIsShowPassword] = useState<boolean>(false);
 
-const passwordChangeHandle = (event: ChangeEvent<HTMLInputElement>) => {
-    setPassword(event.target.value);
+const onChangePassword = (event: ChangeEvent<HTMLInputElement>) => {
+    setPasswordValue(event.target.value);
 };
 
-const toogleShowPassword = () => {
-    setShowPassword(!showPassword);
+const showPassword = () => {
+    setIsShowPassword(true);
 };
+
+const hidePassword = () => {
+    setIsShowPassword(false)
+}
 
     return (
         <div className="homework08-container">
-        <h2>Change password</h2>
-        <Input name="passwordInput" label="Password" placeholder="Enter your password" type="password" value={password} onChange={passwordChangeHandle}/>
-        <Button name="SHOW PASSWORD" onClick={toogleShowPassword}></Button>
-        {showPassword && <div>{password}</div>}
+        <h1>Change password</h1>
+        <Input 
+        name="password" result-container
+        label="Password" 
+        id="password_id"
+        placeholder="Enter your password" 
+        type="password" 
+        value={passwordValue} 
+        onChange={onChangePassword}/>
+        <Button name="SHOW PASSWORD" onClick={showPassword}></Button>
+        <Button name="HIDE PASSWORD" onClick={hidePassword} />
+        {/* Оператор условного рендеринга {isShowPassword && <div className="result-container">{passwordValue}</div>} */}
+        {isShowPassword && <div className="result-container">{passwordValue}</div>}
         </div>);
     }
         
