e26072a8bccdb08bb7fca02fd99133a56de80287
IrinaKholkin <iryna1903@web.de>
2025-03-28 13:08:45 +0100

    add lesson09

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
+// Импорт лекций  
 // импорт компонента Lesson06 по умолчанию
 // при импорте по умолчанию можно использовать другое названия для компонента
 // import Task6 from './lessons/Lesson06/Lesson06';
 // import Lesson06 from './lessons/Lesson06/Lesson06';
-
-// import Consultation04 from "./consultations/Consultation04/Consultation04"
-
-
 // import Lesson07 from "./lessons/Lesson07/Lesson07"
-
-// import Homework07 from "./homeworks/HomeWork07/Homework07"
-
 // import Lesson08 from "./lessons/Lesson08/Lesson08"
 
-import Homework08 from "./homeworks/Homework08/Homework08"
+
+
+// Импорт домашней роботы
+// import Homework07 from "./homeworks/HomeWork07/Homework07"
+// import Homework08 from "./homeworks/Homework08/Homework08"
+
+// Импорт консультаций
+// import Consultation04 from "./consultations/Consultation04/Consultation04"
+
+import Lesson09 from "./lessons/Lesson09/Lesson09"
 
 function App() {
 
@@ -25,8 +28,8 @@ function App() {
       {/* <Homework07 /> */}
       {/* <Lesson08 /> */}
       {/* <Consultation04 /> */}
-
-      <Homework08 />
+ {/* <Homework08 /> */}
+     <Lesson09/>
     </>
   )
 }
