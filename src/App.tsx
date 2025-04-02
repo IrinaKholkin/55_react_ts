e674bad12c302e1da19b2e2bd9d87987339dd4eb
IrinaKholkin <iryna1903@web.de>
2025-04-02 15:20:04 +0200

    add change app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,53 @@
 
 import GlobalStyles from "./styles/GlobalStyles"
 
-// Импорт лекций  
+//Lessons imports
 // импорт компонента Lesson06 по умолчанию
 // при импорте по умолчанию можно использовать другое названия для компонента
 // import Task6 from './lessons/Lesson06/Lesson06';
 // import Lesson06 from './lessons/Lesson06/Lesson06';
 // import Lesson07 from "./lessons/Lesson07/Lesson07"
 // import Lesson08 from "./lessons/Lesson08/Lesson08"
+// import Lesson06 from "./lessons/Lesson06/Lesson06"
 // import Lesson09 from "./lessons/Lesson09/Lesson09"
 // import Lesson10 from "./lessons/Lesson10/Lesson10"
 // import Lesson11 from "./lessons/Lesson11/Lesson11"
-import Homework11 from "./homeworks/Homework11/Homework11"
+import Lesson12 from "./lessons/Lesson12/Lesson12"
 
 
-// Импорт домашней роботы
-// import Homework07 from "./homeworks/HomeWork07/Homework07"
+//Homeworks imports
+// import Homework06 from "./homeworks/Homework06/Homework06"
+// import Homework07 from "./homeworks/Homework07/Homework07"
 // import Homework08 from "./homeworks/Homework08/Homework08"
 // import Homework09 from "./homeworks/Homework09/Homework09"
-// import Homework09AndLesson from "./homeworks/Homework09/Homework09_and_Lesson"
 // import Homework10 from "./homeworks/Homework10/Homework10"
-// import Homework11Lesson from "./homeworks/Homework11Lesson/Homework11Lesson"
+// import Homework11 from "./homeworks/Homework11/Homework11"
 
-// Импорт консультаций
+//Consultations imports
 // import Consultation04 from "./consultations/Consultation04/Consultation04"
 // import Consultation05 from "./consultations/Consultation05/Consultation05"
-
 
 
 function App() {
 
   return (
     <>
-      <GlobalStyles/>
+      <GlobalStyles />
       {/* <Task6 /> */}
       {/* <Lesson06 /> */}
-      {/* <Homework06/> */}
+      {/* <Homework06 /> */}
       {/* <Lesson07 /> */}
-      {/* <Homework07 /> */}
-      {/* <Lesson08 /> */}
+      {/* <Homework07/> */}
+      {/* <Lesson08/> */}
       {/* <Consultation04 /> */}
       {/* <Homework08 /> */}
-      {/* <Lesson09/> */}
-      {/* <Homework09/> */}
-      {/* <Homework09AndLesson/> */}
-      {/* <Lesson10/> */}
-      {/* <Homework10/> */}
-      {/* <Lesson11/> */}
-      <HomeWork11>
-
+      {/* <Lesson09 /> */}
+      {/* <Homework09 /> */}
+      {/* <Lesson10 /> */}
+      {/* <Homework10 /> */}
+      {/* <Lesson11 /> */}
+      {/* <Homework11 /> */}
+      <Lesson12 />
     </>
   )
 }
