6b3d1b522ae64f8756b11b5e5f89d02e3a8998dc
IrinaKholkin <iryna1903@web.de>
2025-04-05 23:45:05 +0200

    add homework14

---
+++ src/homeworks/Homework14/Homework14.tsx
@@ -0,0 +1,12 @@
+import EmployeeForm from "../../components/EmployeeForm/EmployeeForm";
+
+function Homework14(){
+    return (
+        <div>
+            <h1>Employee Form</h1>
+            <EmployeeForm/>
+        </div>
+    );
+}
+
+export default Homework14;
