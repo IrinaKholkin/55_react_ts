c42e7db064c012bd1f0466ace41d9591a1746fc4
IrinaKholkin <iryna1903@web.de>
2025-04-09 22:33:26 +0200

    add homework17

---
+++ src/pages/Clients/styles.ts
@@ -0,0 +1,15 @@
+import styled from "@emotion/styled";
+import { Link } from "react-router-dom";
+
+export const ClientsWrapper = styled.div`
+padding: 40px;
+text-align: center;
+`;
+
+export const CompanyLink = styled(Link)`
+margin: 20px;
+padding: 15px;
+background: #f0f0f0;
+border-radius: 8px;
+cursor: pointer;
+`
